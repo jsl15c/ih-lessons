93cb20ef14088d8ce0d3ab4fea1820fb38ac5e9a
Jarrod Luca <jarrodluca@gmail.com>
2017-06-19 12:30:08 -0400

    update

--- module2/node-intro/app.js
+++ module2/node-intro/app.js
@@ -1,8 +1,36 @@
 var myChalk = require('chalk');
 var myKirbyDance = require('kirby-dance');
 
+// socketIO
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
 
+app.get('/', function(req, res){
+  res.sendFile(__dirname + '/index.html');
+});
 
+io.on('connection', function(socket){
+  console.log('a user connected');
+});
+
+http.listen(3000, function(){
+  console.log('listening on *:3000');
+});
+
+io.on('connection', function(socket){
+  socket.on('chat message', function(msg){
+    console.log('message: ' + msg);
+  });
+});
+
+io.on('connection', function(socket){
+  socket.on('chat message', function(msg){
+    io.emit('chat message', msg);
+  });
+});
+
+// kirby and chalk
 for (var i = 0; i <= 5; i++) {
   var color = myChalk.blue.bgGreen("hello");
   console.log(color);
