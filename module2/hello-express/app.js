d0044073a45a9c318dbf6cfef5f0a667d4643e9a
Jarrod Luca <jarrodluca@gmail.com>
2017-06-21 14:29:13 -0400

    express practice

---
+++ module2/hello-express/app.js
@@ -0,0 +1,10 @@
+const express = require('express');
+
+const app = express();
+
+// ROUTE
+  //    an address (url) you can visit on this application
+app.get('/', (request, response) => {
+  // anonymous function determines the code that is run when visiting the address
+  
+});
